9a614b0745116b0ff421c5b60c5c7120390e2ba0
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2025-03-01 23:39:37 +0530

    🎈 Basics SQL Completed

--- Specific.py
+++ Specific.py
@@ -12,12 +12,16 @@ mycursor=mydb.cursor()
 
 # Selecting  specific things
 
-# mycursor.execute("select class from details_db where name='FEROZ'")
-# mycursor.execute("select name,class from DETAILS_DB")
+mycursor.execute("select class from details_db where name='FEROZ'")
+mycursor.execute("select name,class from DETAILS_DB")
 mycursor.execute("SELECT * FROM DETAILS_DB WHERE roll_no>3")
-# fetch all
+mycursor.execute("select min(roll_no) from DETAILS_DB")
+mycursor.execute("select max(roll_no) from DETAILS_DB")
+mycursor.execute("select name from DETAILS_DB where roll_no between 3 and 5 ")
+mycursor.execute("select count(*),class from DETAILS_DB group by class")
 
+# fetch all
 # res=mycursor.fetchall()
 
 for row in mycursor :
-    print(row)
+    print(row) 
