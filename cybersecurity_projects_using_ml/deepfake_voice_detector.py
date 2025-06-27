5baa411fda41a77cc04d7fe11b5c9a26dbc502af
FairozAhmadSheikh <134375617+FairozAhmadSheikh@users.noreply.github.com>
2025-06-27 11:05:20 +0530

    c2

--- cybersecurity_projects_using_ml/deepfake_voice_detector.py
+++ cybersecurity_projects_using_ml/deepfake_voice_detector.py
@@ -1,1 +1,10 @@
 # pip install librosa numpy matplotlib scikit-learn tensorflow
+import os
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, TimeDistributed, LSTM, Dense, Flatten, Dropout
